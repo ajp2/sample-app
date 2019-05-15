cc9647674e57171083e88b559f6dac8080f3b0e1
AJP <arvind.puvi@gmail.com>
2019-05-15 22:43:34 +1000

    Seed db

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -47,6 +47,7 @@ namespace SportsStore
                     name: "default",
                     template: "{controller=Product}/{action=List}/{id?}");
             });
+            SeedData.EnsurePopulated(app);
         }
     }
 }
